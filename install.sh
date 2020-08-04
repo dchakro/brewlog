f67e85057547da42b5ea21e3b3c9997ec17a3b41
robocopAlpha <35454738+robocopAlpha@users.noreply.github.com>
2020-08-04 13:37:25 +0300

    Trying to install to the same place as homebrew

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+BREWLOCATION=$(command -v brew)
+echo "$BREWLOCATION/brewlog"
+if [ "$?" -ne "0" ]; then
+    echo "Homebrew/Linux brew installation not detected in your PATH."
+    exit 1
+else
+    BREWLOCATION= $(dirname "$BREWLOCATION")
+fi
+
 if [ -w /usr/local/bin ]; then
-   curl -sSL 'https://raw.githubusercontent.com/robocopAlpha/brewlog/master/brewlog.sh' > /usr/local/bin/brewlog
-   chmod +x /usr/local/bin/brewlog
+   curl -sSL 'https://raw.githubusercontent.com/robocopAlpha/brewlog/master/brewlog.sh' > $BREWLOCATION/brewlog
+   chmod +x $BREWLOCATION/brewlog
 else
   curl -sSL 'https://raw.githubusercontent.com/robocopAlpha/brewlog/master/brewlog.sh' >| brewlog.sh
-  echo "user $(whoami) cannot write to /usr/local/bin."
+  echo "user $(whoami) cannot write to $BREWLOCATION"
   read -r -p "use sudo (y/N): " choice
   shopt -q nocasematch;
   if [ "$?" -eq "1" ]; then
@@ -13,11 +22,11 @@ else
   fi
   if [[ "$choice" =~ ^(yes|y)$ ]]
   then
-    sudo mv brewlog.sh /usr/local/bin/brewlog
-    sudo chmod +x /usr/local/bin/brewlog
+    sudo mv brewlog.sh $BREWLOCATION/brewlog
+    sudo chmod +x $BREWLOCATION/brewlog
     sudo -k
   else
-    echo "cannot install /usr/local/bin/brewlog without sudo rights"
+    echo "cannot install $BREWLOCATION/brewlog without sudo rights"
     exit 1;
   fi
   if [ "$flag" -eq "1" ]; then
