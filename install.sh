6296379a5525eb623b2347536c215c6405b2188a
robocopAlpha <35454738+robocopAlpha@users.noreply.github.com>
2020-08-04 13:50:33 +0300

    change order of checking shopt nocasematch

--- install.sh
+++ install.sh
@@ -15,14 +15,14 @@ if [ -w /usr/local/bin ]; then
 brewlog has been installed as $(command -v brewlog). 
 You can start with brewlog --help %s\n"
 else
-  curl -sSL 'https://raw.githubusercontent.com/robocopAlpha/brewlog/master/brewlog.sh' >| brewlog.sh
-  echo "user $(whoami) cannot write to $BREWLOCATION"
-  read -r -p "use sudo (y/N): " choice
   shopt -q nocasematch;
   if [ "$?" -eq "1" ]; then
     shopt -s nocasematch
     flag=1
   fi
+  curl -sSL 'https://raw.githubusercontent.com/robocopAlpha/brewlog/master/brewlog.sh' >| brewlog.sh
+  echo "user $(whoami) cannot write to $BREWLOCATION"
+  read -r -p "use sudo (y/N): " choice
   if [[ "$choice" =~ ^(yes|y)$ ]]
   then
     sudo mv brewlog.sh $BREWLOCATION/brewlog
